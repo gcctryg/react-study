fae8707a6f1b413fc068c5780b036c582bfea974
gcctryg <gcctryg@gmail.com>
2023-11-12 12:47:58 -0800

    pass by children and ractnode

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 //import Message from './Message'
+import Alert from "./components/Alert"
 import ListGroup from "./components/listGroup"
 
 function App() {
-  let items = [
-    "Pokemon XD",
-    "Resident Evil 4",
-    "The Last of Us",
-    "The Lion King",
-  ];
+  //ListGroup
+  // let items = [
+  //   "Pokemon XD",
+  //   "Resident Evil 4",
+  //   "The Last of Us",
+  //   "The Lion King",
+  // ];
 
-  const handleSelectItem = (item:string) => {
-    console.log(item)
-  }
+  // const handleSelectItem = (item:string) => {
+  //   console.log(item)
+  // }
 
-  return <div><ListGroup items={items} heading={"Games"} onSelectItem={handleSelectItem} /></div>
+  // return <div><ListGroup items={items} heading={"Games"} onSelectItem={handleSelectItem} /></div>
+
+  return ( <div>
+    <Alert>
+      Hellow <span>World</span>
+    </Alert>
+  </div>)
+
 }
 
 export default App
